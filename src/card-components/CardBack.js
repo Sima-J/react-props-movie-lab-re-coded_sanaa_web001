8904062aa1778d9d6182cc0c81478f7773ee0129
blessed-installation-48cc <sima.jazaa@gmail.com>
2021-03-12 20:37:08 +0000

    Automatically backed up by Learn

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -20,8 +20,8 @@ export default class CardBack extends Component {
       <div className="card-back">
         <h3 className="title">{this.props.title}</h3>
         <span />
-        { /* your rating element should go here -- you can invoke methods within JSX, à la: this.myMethod() */ }
-        <span />
+				{this.generateRatingElement()}
+				<span />
         <h5 className="genres"></h5>
       </div>
     )
