d5a4e262cdd0c1c640a44505b98ad99fc168ea43
blessed-installation-48cc <sima.jazaa@gmail.com>
2021-03-12 20:35:25 +0000

    Automatically backed up by Learn

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -13,8 +13,7 @@ export default class CardBack extends Component {
   generateRatingElement = () => {
     if (this.props.IMDBRating !== null) {
 			return <img src={imgMapper[this.props.IMDBRating]} alt="" />;
-		}
-		return <h4>No Rating Found</h4>;
+		} return <h4>No Rating Found</h4>;
   }
 
   render() {
